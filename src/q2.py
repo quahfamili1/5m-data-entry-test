f6fe273e90edfb3fd90d9e8f8f93e5c500a6ee59
quahfamili1 <b00n12@gmail.com>
2025-06-08 15:10:09 +0800

    Update q2.py

--- src/q2.py
+++ src/q2.py
@@ -5,10 +5,30 @@ def find_and_replace(lst, find_val, replace_val):
     - lst must be a list.
     - Return the modified list.
     """
-    return
+    if not isinstance(lst, list):
+        print("Error: Input 'lst' must be a list.")
+        return None
+
+    modified_list = []
+    for item in lst:
+        if item == find_val:
+            modified_list.append(replace_val)
+        else:
+            modified_list.append(item)
+            
+    return modified_list
 
 
 # Task 2
 # Invoke the function "find_and_replace" using the following scenarios:
 # - [1, 2, 3, 4, 2, 2], 2, 5
 # - ["apple", "banana", "apple"], "apple", "orange"
+
+print("Scenario 1: ")
+result1 = find_and_replace([1, 2, 3, 4, 2, 2], 2, 5)
+print(f"Modified list: {result1}")
+
+
+print("\nScenario 2: ")
+result2 = find_and_replace(["apple", "banana", "apple"], "apple", "orange")
+print(f'Modified list: {result2}')
