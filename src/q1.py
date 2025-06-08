14e35b541e9808779d416f7c8ef298f96f0fe592
quahfamili1 <b00n12@gmail.com>
2025-06-08 14:53:55 +0800

    Update q1.py

--- src/q1.py
+++ src/q1.py
@@ -6,23 +6,23 @@ def swap(x, y):
     - Returns -1 if x and y are not numeric.
     - Prints the swapped values if both x and y are numeric.
     """
-
+    
+    result = "" 
     if not isinstance(x, (int, float)) or not isinstance(y, (int, float)):
-        return -1
-
-    # Print original values 
-    print(f"Original values: x = {x}, y = {y}")
+        result = "-1"
 
     # Swapping logic
-    x,y = y,x
-
-    # Print the swapped values
-    print(f"Swapped values:  x = {x}, y = {y}")
-
+    else:
+        x,y = y,x
+        result = "Swapped values:  x = " + str(x) + ", y = " + str(y) 
+        
+    return result
+    
+    
 print("Scenario 1: ")
 result1 = swap("Apple", 10)
-print({result1})
+print(result1)
 
 print("\nScenario 2: ")
 result2 = swap(9, 17)
-print({result2})
+print(result2)
